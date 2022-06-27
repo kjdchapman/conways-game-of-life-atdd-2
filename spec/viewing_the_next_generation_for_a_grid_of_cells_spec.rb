1cba6af9dfc45b6e0feebded323875699f61b66a
kjdchapman <kjdchapman@gmail.com>
2022-06-27 11:43:09 +0100

    Add passing acceptance spec for a 2x2 grid of live cells

--- spec/viewing_the_next_generation_for_a_grid_of_cells_spec.rb
+++ spec/viewing_the_next_generation_for_a_grid_of_cells_spec.rb
@@ -1,24 +1,33 @@
-describe 'viewing the next generation for a grid of cells' do
-  describe 'a 4x4 grid with no cells' do
-    it 'remains empty' do
-      empty_grid = [
-        [:dead, :dead, :dead, :dead],
-        [:dead, :dead, :dead, :dead],
-        [:dead, :dead, :dead, :dead],
-        [:dead, :dead, :dead, :dead]
-      ]
+describe 'a 4x4 grid with no cells' do
+  describe 'still life' do
+    describe 'a grid of dead cells' do
+      it 'remains empty' do
+        empty_grid = [
+          [:dead, :dead, :dead, :dead],
+          [:dead, :dead, :dead, :dead],
+          [:dead, :dead, :dead, :dead],
+          [:dead, :dead, :dead, :dead]
+        ]
 
-      expect(next_conway_generation(empty_grid)).to eq(empty_grid)
+        expect(next_conway_generation(empty_grid)).to eq(empty_grid)
+      end
+    end
+
+    describe 'a 2x2 box of cells on a 4x4 grid' do
+      it 'remains the same' do
+        box_grid = [
+          [:dead, :dead, :dead, :dead],
+          [:dead, :live, :live, :dead],
+          [:dead, :live, :live, :dead],
+          [:dead, :dead, :dead, :dead]
+        ]
+
+        expect(next_conway_generation(box_grid)).to eq(box_grid)
+      end
     end
   end
 
   def next_conway_generation(grid)
     grid
-  end
-
-  xdescribe 'still life' do
-    describe 'a 2x2 box of cells on a 4x4 grid' do
-      # use :live for alive cells
-    end
   end
 end
